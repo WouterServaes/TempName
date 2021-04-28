d84a10734e06744b76a4a80483bd4fb202caf3e0
Wouter Servaes <wouterservaes@protonmail.com>
2021-04-28 23:57:01 +0200

    Minor improvement in Logger

--- Minigin/Logger.h
+++ Minigin/Logger.h
@@ -72,15 +72,17 @@ private:
 		{
 			//https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
 #if _DEBUG
-			const auto msgBoxmsg{ (msg + static_cast<std::string>("\n\n Retry: breaks program\n Cancel: continues program (may result in more crashes)")) };
+			const auto msgBoxmsg{ (msg + static_cast<std::string>("\n\n Try again: puts breakpoint in program\n Continue: continues program (may result in more crashes)\nCancel: closes program (beware leaks)")) };
 
-			const auto msgBoxReturn{ MessageBoxA(0, msgBoxmsg.c_str(), "[ERROR]", MB_ICONERROR | MB_RETRYCANCEL | MB_TOPMOST) };
-			if (msgBoxReturn == IDRETRY)
+			const auto msgBoxReturn{ MessageBoxA(0, msgBoxmsg.c_str(), "[ERROR]", MB_ICONERROR | MB_CANCELTRYCONTINUE | MB_TOPMOST | MB_DEFBUTTON2) };
+			if (msgBoxReturn == IDTRYAGAIN)
 			{
 				// is there a way to step through the call stack through code? breakpoint gets put here, but the user should see the actual error.
 				//
 				__debugbreak();
 			}
+			else if (msgBoxReturn == IDCANCEL)
+				exit(-1);
 #else
 
 			const auto msgBoxmsg{ (static_cast<std::string>("Critical error:\n") + msg + static_cast<std::string>("\n\n Click OK to close program")) };
